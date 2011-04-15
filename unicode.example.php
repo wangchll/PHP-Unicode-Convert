ec26981848892995e810a731c9dcd62e71fda614
wangchll <wangchong1985@gmail.com>
2011-04-15 20:32:28 +0800

    Add example

---
+++ unicode.example.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *  Demo
+ * 
+ * @author wang chong(wangchong1985@gmail.com)
+ * @link http://www.wangchong.org
+ * @version 1.0.0 (2011-04-15)
+ * @package php-Unicode
+ */
+
+include 'unicode.class.php';
+
+$str = '这是一个测试Demo!~！wangchong1985@gmail.com';
+
+$convert = new Unicode();
+
+//convert to Unicode
+var_dump($str = $convert->str_to_unicode($str, 'UTF-8'));
+
+//convert to UTF-8
+var_dump($str = $convert->str_from_unicode($str, 'UTF-8'));
